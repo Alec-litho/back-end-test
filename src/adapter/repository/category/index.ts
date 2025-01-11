9d29a20f03b0c42730c661c96d7e5609b316553a
oleg <zustzero123@gmail.com>
2025-01-11 13:49:01 +0300

    created delete, create, get, list and update Upvote functions and exported through index.ts file

---
+++ src/adapter/repository/category/index.ts
@@ -0,0 +1,23 @@
+import { AdapterParams } from '@/adapter/types'
+import { buildCreate, Create } from './create'
+import { buildDelete, Delete } from './delete'
+import { buildGet, Get } from './get'
+
+type Params = Pick<AdapterParams, 'db'>
+
+export type UpvoteRepository = {
+  create: Create,
+  deleteUpvote:Delete,
+  get:Get
+}
+export const buildUpvoteRepository = (params: Params): UpvoteRepository=>{
+  const create = buildCreate(params)
+  const deleteUpvote = buildDelete(params)
+  const get = buildGet(params)
+
+  return {
+    create,
+    deleteUpvote,
+    get
+  }
+}
